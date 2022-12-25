b04eba0b3366ba8127f81e6ffa95cdf91a71933f
Emilia Carvajal <emilia.carvajalm@gmail.com>
2022-12-24 23:05:39 -0400

    Adding code od sum positive numbers on dev branch

---
+++ SumPositiveNumbers/SumPositiveNumb/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Por favor ingrese el numero:");
+    string numeroComoString = Console.ReadLine();    
+    // string a int
+    int numero = Int32.Parse(numeroComoString);
+    int suma =0;
+    // verificar q el numero sea positivo y se hace la suma
+    while(numero >= 0)
+    {
+      suma= suma + numero;    
+      Console.WriteLine ("Por favor ingrese otro numero:");
+      string numeroComoString2 = Console.ReadLine();
+      // string a int
+      numero = Int32.Parse(numeroComoString2);
+    }
+    Console.WriteLine ("El valor de la suma de numeros positivos es:" +suma);
+    
+  }
+}
